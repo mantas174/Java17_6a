fef7c9f7622926b015cb6fd2e645bec73c36ac94
Mantas <mantas.muzzz@gmail.com>
2018-02-20 10:50:51 +0200

    fist commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// 6a.
+        //​
+        // Sukurti žaidimą kurio tikslas atspėti skaičių nuo 1 iki 10. Kaskart spėjant skaičių ir
+        //neatspėjus yra generuojamas naujas skaičius.  Žaidimas baigiasi kuomet skaičius yra
+        //atspėjamas arba vartotojas kaip spėjamą skaičių įveda 0.
+        //            Random rand = new Random();
+        //int randomNumber  = rand.nextInt(10) + 1;
+
+
+    }
+}
